500f4e6dded3e5ec563118f820766848891092d1
hummingbird <peakdev1224@outlook.com>
2023-05-04 06:00:24 +0300

    commit test

--- react/src/comment-counts.tsx
+++ react/src/comment-counts.tsx
@@ -1,38 +1,37 @@
 import React, { useEffect } from "react";
 import { addScriptIfNotAdded } from "./helper";
 
-
-export function loadCommentCounts(
-    options: {
-        "website-id"?: number,
-        mode?: 'text' | 'number',
-        language?: string,
-    } = {},
-    callback: ((count: number | string, el: Element) => string | number) | null = null
+export function loadCommentCounts( //commit test
+  options: {
+    "website-id"?: number;
+    mode?: "text" | "number";
+    language?: string;
+  } = {},
+  callback:
+    | ((count: number | string, el: Element) => string | number)
+    | null = null
 ) {
-    customElements.whenDefined('hyvor-talk-comment-count').then(() => {
-        (window as any).hyvorTalkCommentCounts.load(options, callback);
-    });
+  customElements.whenDefined("hyvor-talk-comment-count").then(() => {
+    (window as any).hyvorTalkCommentCounts.load(options, callback);
+  });
 }
 
-
 export function CommentCount(props: {
-    'page-id': string | number,
-    'website-id'?: number,
-    mode?: 'text' | 'number',
-    language?: string,
+  "page-id": string | number;
+  "website-id"?: number;
+  mode?: "text" | "number";
+  language?: string;
 
-    loading?: 'manual'
+  loading?: "manual";
 }) {
+  useEffect(() => {
+    addScriptIfNotAdded("https://talk.hyvor.com/embed/comment-counts.js");
 
-    useEffect(() => {
-        addScriptIfNotAdded('https://talk.hyvor.com/embed/comment-counts.js');
+    if (props.loading === "manual") {
+      return;
+    }
+    loadCommentCounts();
+  }, []);
 
-        if (props.loading === 'manual') {
-            return;
-        }
-        loadCommentCounts();
-    }, []);
-
-    return <hyvor-talk-comment-count {...props} />
+  return <hyvor-talk-comment-count {...props} />;
 }
--- react/src/comments.tsx
+++ react/src/comments.tsx
@@ -1,62 +1,56 @@
-import React, { useEffect, useRef } from 'react';
-import type { Settings, Translations } from '../../types';
-import { addScriptIfNotAdded } from './helper';
+import React, { useEffect, useRef } from "react";
+import type { Settings, Translations } from "../../types";
+import { addScriptIfNotAdded } from "./helper";
 
 interface CommentsProps {
+  "website-id": number;
 
-    "website-id": number,
-    
-    "page-id"?: number | string,
-    "page-url"?: string,
-    "page-title"?: string,
-    "page-language"?: string,
-    "page-author"?: string,
+  "page-id"?: number | string;
+  "page-url"?: string;
+  "page-title"?: string;
+  "page-language"?: string;
+  "page-author"?: string;
 
-    "sso-user"?: string,
-    "sso-hash"?: string,
+  "sso-user"?: string;
+  "sso-hash"?: string;
 
-    colors?: 'light' | 'dark' | 'os',
-    loading?: 'default' | 'lazy' | 'manual',
+  colors?: "light" | "dark" | "os";
+  loading?: "default" | "lazy" | "manual";
 
-    settings?: Partial<Settings>,
-    translations?: Partial<Translations>
-
+  settings?: Partial<Settings>;
+  translations?: Partial<Translations>;
 }
 
 export function Comments(props: CommentsProps) {
+  const ref = useRef<null | HTMLDivElement>(null); // commit test
 
-    const ref = useRef<null | HTMLDivElement>(null);
+  useEffect(() => {
+    addScriptIfNotAdded("https://talk.hyvor.com/embed/embed.js");
 
-    useEffect(() => {
+    customElements.whenDefined("hyvor-talk-comments").then(() => {
+      const comments = document.createElement(
+        "hyvor-talk-comments"
+      ) as HTMLElement & {
+        settings: Partial<Settings>;
+        translations: Partial<Translations>;
+      };
 
-        addScriptIfNotAdded('https://talk.hyvor.com/embed/embed.js');
+      for (const [key, value] of Object.entries(props)) {
+        if (value !== undefined) {
+          if (key === "settings" || key === "translations") {
+            comments[key] = value;
+          } else {
+            comments.setAttribute(key, value);
+          }
+        }
+      }
 
-        customElements.whenDefined('hyvor-talk-comments').then(() => {
+      if (ref.current) {
+        ref.current.innerHTML = "";
+        ref.current.appendChild(comments);
+      }
+    });
+  }, []);
 
-            const comments = document.createElement('hyvor-talk-comments') as HTMLElement & {
-                settings: Partial<Settings>,
-                translations: Partial<Translations>
-            };
-    
-            for (const [key, value] of Object.entries(props)) {
-                if (value !== undefined) {
-                    if (key === 'settings' || key === 'translations') {
-                        comments[key] = value;
-                    } else {
-                        comments.setAttribute(key, value);
-                    }
-                }
-            }
-    
-            if (ref.current) {
-                ref.current.innerHTML = '';
-                ref.current.appendChild(comments);
-            }
-
-        });
-
-    }, []);
-
-    return <div ref={ref}></div>
-
+  return <div ref={ref}></div>;
 }
